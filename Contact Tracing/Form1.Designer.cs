8e5073f833918d8ef6fce054bbb31d4b21b57f69
thisismic <micmic.inocentes@gmail.com>
2021-06-07 17:03:39 +0800

    just changed the bg color

--- Contact Tracing/Form1.Designer.cs
+++ Contact Tracing/Form1.Designer.cs
@@ -118,6 +118,7 @@ namespace Contact_Tracing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(416, 155);
             this.Controls.Add(this.txtboxPronouns);
             this.Controls.Add(this.txtboxAge);
